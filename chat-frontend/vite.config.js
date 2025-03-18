dca605f778607df0936d2aeae28e05729629c337
Bahl-Shaurya <shauryab@gigmos.com>
2025-03-18 23:02:48 +0530

    Chat Application using React+Vite Framework

---
+++ chat-frontend/vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000, // Change to 3000 if 5173 is conflicting
+  },
+});
